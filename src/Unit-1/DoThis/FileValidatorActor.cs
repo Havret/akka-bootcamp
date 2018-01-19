5aab5fad0c4fcfbaa1a57b07b203f0c14e0fb63d
Havret <h4vret@gmail.com>
2018-01-19 17:10:44 +0100

    Unit 1 - Lesson 4

---
+++ src/Unit-1/DoThis/FileValidatorActor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class FileValidatorActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+        private readonly IActorRef _tailCoordinatorActor;
+
+        public FileValidatorActor(IActorRef consoleWriterActor, IActorRef tailCoordinatorActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+            _tailCoordinatorActor = tailCoordinatorActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                // signal that the user needs to supply an input, as previously
+                // received input was blank
+                _consoleWriterActor.Tell(new Messages.NullInputError("No input received."));
+            }
+            else
+            {
+                var valid = IsFileUri(msg);
+                if (valid)
+                {
+                    // signal successful input
+                    _consoleWriterActor.Tell(new Messages.InputSuccess($"Starting processing for {msg}"));
+
+                    // start coordinator
+                    _tailCoordinatorActor.Tell(new TailCoordinatorActor.StartTail(msg, _consoleWriterActor));
+                }
+                else
+                {
+                    // signal that input was bad
+                    _consoleWriterActor.Tell(new Messages.ValidationError($"{msg} is not an existing URI on disk."));
+
+                    // tell sender to continue doing its thing (whatever that
+                    // may be, this actor doesn't care)
+                    Sender.Tell(new Messages.ContinueProcessing());
+                }
+            }
+
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private static bool IsFileUri(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/TailCoordinatorActor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class TailCoordinatorActor : UntypedActor
+    {
+        #region Message types
+
+        /// <summary>
+        /// Start tailing the file at user-specified path.
+        /// </summary>
+        public class StartTail
+        {
+            public StartTail(string filePath, IActorRef reporterActor)
+            {
+                FilePath = filePath;
+                ReporterActor = reporterActor;
+            }
+
+            public string FilePath { get; }
+
+            public IActorRef ReporterActor { get; }
+        }
+
+        /// <summary>
+        /// Stop tailing the file at user-specified path.
+        /// </summary>
+        public class StopTail
+        {
+            public StopTail(string filePath)
+            {
+                FilePath = filePath;
+            }
+
+            public string FilePath { get; }
+        }
+
+        #endregion
+
+        protected override void OnReceive(object message)
+        {
+            if (message is StartTail startTail)
+            {
+                Context.ActorOf(Props.Create(() => new TailActor(startTail.ReporterActor, startTail.FilePath)));
+            }
+        }
+
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return new OneForOneStrategy(
+                maxNrOfRetries: 10,
+                withinTimeRange: TimeSpan.FromSeconds(30),
+                localOnlyDecider: x =>
+                {
+                    //Maybe we consider ArithmeticException to not be application critical
+                    //so we just ignore the error and keep going.
+                    if (x is ArithmeticException) return Directive.Resume;
+
+                    //Error that we cannot recover from, stop the failing actor
+                    else if (x is NotSupportedException) return Directive.Stop;
+
+                    //In all other cases, just restart the failing actor
+                    else return Directive.Restart;
+                }
+            );
+        }
+    }
+}
