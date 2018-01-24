3b59323df970d948efedae10393c10aa38099eb2
Havret <h4vret@gmail.com>
2018-01-24 14:11:00 +0100

    Unit 2 - Lesson 3

---
+++ src/Unit-2/DoThis/Actors/PerformanceCounterActor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Remoting.Contexts;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    /// Actor responsible for monitoring a specific <see cref="PerformanceCounter"/>
+    /// </summary>
+    public class PerformanceCounterActor : UntypedActor
+    {
+        private readonly string _seriesName;
+        private readonly Func<PerformanceCounter> _performanceCounterGenerator;
+        private PerformanceCounter _counter;
+
+        private readonly HashSet<IActorRef> _subscriptions;
+        private readonly ICancelable _cancelPublishing;
+
+        public PerformanceCounterActor(string seriesName, Func<PerformanceCounter> performanceCounterGenerator)
+        {
+            _seriesName = seriesName;
+            _performanceCounterGenerator = performanceCounterGenerator;
+            _subscriptions = new HashSet<IActorRef>();
+            _cancelPublishing = new Cancelable(Context.System.Scheduler);
+        }
+
+        protected override void PreStart()
+        {
+            // create a new instance of the performance counter
+            _counter = _performanceCounterGenerator();
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromMilliseconds(250),
+                TimeSpan.FromMilliseconds(250),
+                Self,
+                new GatherMetrics(),
+                Self,
+                _cancelPublishing);
+        }
+
+        protected override void PostStop()
+        {
+            try
+            {
+                // terminate scheduled task
+                _cancelPublishing.Cancel(false);
+                _counter.Dispose();
+            }
+            catch
+            {
+                // don't care about additional "ObjectDisposed" exceptions
+            }
+            finally
+            {
+                base.PostStop();
+            }
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is GatherMetrics)
+            {
+                // publish latest counter value to all subscribers
+                var metric = new Metric(_seriesName, _counter.NextValue());
+                foreach (var sub in _subscriptions)
+                    sub.Tell(metric);
+            }
+            else if (message is SubscribeCounter subscribeCounter)
+            {
+                // add a subscription for this counter
+                // (it's parent job to filter by counter types)
+                _subscriptions.Add(subscribeCounter.Subscriber);
+            }
+            else if (message is UnsubscribeCounter unsubscribeCounter)
+            {
+                // remove a subscription from this counter
+                _subscriptions.Remove(unsubscribeCounter.Subscriber);
+            }
+        }
+    }
+}
